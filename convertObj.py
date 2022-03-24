05a8f4e4c7b2fee0a19db772d9eb2f2d6eae1d06
Keith Sloan <keith@sloan-home.co.uk>
2022-03-24 21:28:12 +0000

    Update

--- convertObj.py
+++ convertObj.py
@@ -18,6 +18,7 @@
 # *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 *
 # *   USA                                                                  *
 # *                                                                        *
+# *                                                                        *
 # ***************************************************************************
 __title__ = "convertOBJ - Covert Obj to GDML Tessellated"
 __author__ = "Keith Sloan <keith@sloan-home.co.uk>"
